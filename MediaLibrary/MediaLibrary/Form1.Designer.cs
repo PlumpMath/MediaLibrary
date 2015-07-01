a6695daa4e3a29c2ed25d5a8c1984715aabf8d58
balajisankar1 <balajisankar1@gmail.com>
2015-07-01 10:37:33 -0400

    Added ability to choose a file though a basic dialogue and play it

--- MediaLibrary/MediaLibrary/Form1.Designer.cs
+++ MediaLibrary/MediaLibrary/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtSekectedFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(12, 32);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtSekectedFile
+            // 
+            this.txtSekectedFile.Location = new System.Drawing.Point(107, 35);
+            this.txtSekectedFile.Name = "txtSekectedFile";
+            this.txtSekectedFile.Size = new System.Drawing.Size(494, 20);
+            this.txtSekectedFile.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 78);
+            this.Controls.Add(this.txtSekectedFile);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtSekectedFile;
     }
 }
 
--- MediaLibrary/MediaLibrary/Form1.cs
+++ MediaLibrary/MediaLibrary/Form1.cs
@@ -15,6 +15,28 @@ namespace MediaLibrary
         public Form1()
         {
             InitializeComponent();
+
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            int variable = 1 + 1;
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Video Files (*.mp4, *.avi)|*.mp4;*.avi";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileToPlay = ofd.FileName;
+                txtSekectedFile.Text = fileToPlay;
+                AXVLC.VLCPlugin alxplugin1 = new AXVLC.VLCPlugin();
+                alxplugin1.addTarget("file:///" + fileToPlay, null, AXVLC.VLCPlaylistMode.VLCPlayListInsert, 0);
+                alxplugin1.play();
+            }
         }
     }
 }
