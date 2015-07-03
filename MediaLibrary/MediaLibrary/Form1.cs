0950daea0919db4b489a436e5f13a7ed830c50ca
balajisankar1 <balajisankar1@gmail.com>
2015-07-03 12:34:23 -0400

    Added beginnings of Datastore using SqLite

--- MediaLibrary/MediaLibrary/Form1.cs
+++ MediaLibrary/MediaLibrary/Form1.cs
@@ -7,20 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace MediaLibrary
 {
     public partial class Form1 : Form
     {
         AXVLC.VLCPlugin alxplugin1 = new AXVLC.VLCPlugin();
+        SQLiteConnection filesLibraryConnection;
+        const string DBName = "LibraryFiles.sqlite";
+        const int DBVersion = 1;
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-            int variable = 1 + 1;
+            if (!System.IO.File.Exists(DBName))
+            {
+                SQLiteConnection.CreateFile(DBName);
+            }
+            filesLibraryConnection = new SQLiteConnection("Data Source="+ DBName + ";Version=3;");
+            filesLibraryConnection.Open();
+            string commandString = "SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'Version\'";
+            SQLiteCommand command = new SQLiteCommand(commandString, filesLibraryConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            if(reader.HasRows) {
+                commandString = "SELECT number FROM Version order by number desc";
+                command = new SQLiteCommand(commandString, filesLibraryConnection);
+                reader = command.ExecuteReader();
+                while(reader.Read())
+                    if (!(reader.GetInt32(0) == DBVersion))
+                    {
+                        Application.Exit();
+                        // TODO Implement Upgrader
+                    }
+            }
+            else {
+                commandString = "CREATE TABLE Version (number INT32)";
+                command = new SQLiteCommand(commandString, filesLibraryConnection);
+                command.ExecuteNonQuery();
+                commandString = "insert into Version (number) values (" + DBVersion + ")";
+                command = new SQLiteCommand(commandString, filesLibraryConnection);
+                command.ExecuteNonQuery();
+            }
         }
 
         private void btnSelectFile_Click(object sender, EventArgs e)
